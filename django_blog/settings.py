079341ae7c299a1e70a40cde1876310144f0d33f
Kevin Amarbayar <amarbayar.kevin@gmail.com>
2020-10-06 23:46:53 -0700

    added env variables

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -21,12 +21,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#q2qil!r%qz%=r*t3^wiun#m=p$^mp-#sbu6)q0*-vg5990s3+'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+# DEBUG = os.environ.get('DEBUG_VALUE')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'blog-app-sdkevin.herokuapp.com'
+]
 
 
 # Application definition
